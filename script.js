a5bba7dc35d52249edeb5a944a324f96d2cd3af6
CRW405 <134819932+CRW405@users.noreply.github.com>
2025-12-03 08:49:56 -0600

    deleted unused code

--- script.js
+++ script.js
@@ -23,65 +23,6 @@ function draw(newX, newY) {
 // dial setup
 const dialLeft = document.getElementById('dial-left');
 const dialRight = document.getElementById('dial-right');
-// let leftAngle = 0;
-// let rightAngle = 0;
-// let leftDragging = false;
-// let rightDragging = false;
-//
-// function getMovement(dial, mouseX, mouseY) {
-//   const bounds = dial.getBoundingClientRect();
-//   const centerX = bounds.left + bounds.width / 2;
-//   const centerY = bounds.top + bounds.height / 2;
-//   const deltaX = mouseX - centerX;
-//   const deltaY = mouseY - centerY;
-//   return Math.atan2(deltaY, deltaX);
-// }
-//
-// dialLeft.addEventListener('mousemove', (e) => {
-//   leftDragging = true;
-//   leftAngle = getMovement(dialLeft, e.clientX, e.clientY);
-// });
-//
-// dialLeft.addEventListener('mouseup', () => {
-//   leftDragging = false;
-// });
-//
-// dialRight.addEventListener('mousemove', (e) => {
-//   rightDragging = true;
-//   rightAngle = getMovement(dialRight, e.clientX, e.clientY);
-// });
-//
-// dialRight.addEventListener('mouseup', () => {
-//   rightDragging = false;
-// });
-//
-//
-// document.addEventListener('mousemove', (e) => {
-//   if (leftDragging) {
-//     const newLeftAngle = getMovement(dialLeft, e.clientX, e.clientY);
-//     const leftDelta = newLeftAngle - leftAngle;
-//
-//     const newX = x + leftDelta;
-//     draw(newX, y);
-//
-//     leftAngle = newLeftAngle;
-//   }
-//
-//   if (rightDragging) {
-//     const newRightAngle = getMovement(dialRight, e.clientX, e.clientY);
-//     const rightDelta = newRightAngle - rightAngle;
-//
-//     const newY = y + rightDelta;
-//     draw(x, newY);
-//
-//     rightAngle = newRightAngle;
-//   }
-// });
-//
-// document.addEventListener('mouseup', () => {
-//   leftDragging = false;
-//   rightDragging = false;
-// });
 
 // scroll setup
 dialLeft.addEventListener('wheel', (e) => {
